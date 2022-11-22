64653a48239f8d8c25b5b0d4504bcee68e5177a6
Rolando <rolando.code@gmail.com>
2022-11-22 17:31:54 -0600

    Employee types

---
+++ OpenRestRestaurant.Server/OpenRestRestaurant-models/Catalogs/EmployeeType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenRestRestaurant_models.Catalogs
+{
+    public class EmployeeType
+    {
+        public const int SystemAdministrator = 0;
+    }
+}
